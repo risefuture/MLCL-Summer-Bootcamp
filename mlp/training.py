f4e0188d247b672a6d60ebb15a43e8a9e38bcfd4
risefuture <135511093+risefuture@users.noreply.github.com>
2023-07-03 14:01:54 +0900

    Add files via upload

---
+++ mlp/training.py
@@ -0,0 +1,61 @@
+# training.py
+import yaml
+import torch
+import torch.optim as optim
+from torch.nn import MSELoss
+from utils.model_mlp import MLP
+from utils.dataset import GasDataset
+from torch.utils.data import DataLoader
+
+# Load the configuration file
+with open('./configs/configs.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# TODO: Initialize the model, loss, and optimizer
+epochs = 20
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
+criterion = nn.MSELoss()
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+
+# Create the training and validation datasets and dataloaders
+train_dataset = GasDataset(config['paths']['data'], start_year=None, end_year=2017)
+train_loader = DataLoader(train_dataset, batch_size=config['training']['batch_size'], shuffle=True)
+
+val_dataset = GasDataset(config['paths']['data'], start_year=2018, end_year=2019)
+val_loader = DataLoader(val_dataset, batch_size=config['training']['batch_size'], shuffle=False)
+
+# Training
+for epoch in range(config['training']['num_epochs']):
+    # Training phase
+    model.train()
+    train_losses = []
+    for inputs, labels in train_loader:
+        # TODO: Implement the training step here
+        model.train()
+        optimizer.zero_grad()
+        y_pred = model(inputs)
+        loss = criterion(y_pred.squeeze(), labels)
+        loss.backward()
+        optimizer.step()
+
+    # Validation phase
+    model.eval()
+    val_losses = []
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            # TODO: Implement the validation step here
+            y_pred_val = model(inputs)
+            val_loss = criterion(y_pred_val.squeeze(), labels)
+            val_losses.append(val_loss.item())
+            
+
+    print(f'Epoch {epoch+1}/{config["training"]["num_epochs"]}, '
+          f'Train Loss: {sum(train_losses)/len(train_losses)}, '
+          f'Val Loss: {sum(val_losses)/len(val_losses)}')
+
+# TODO: Save the model
+torch.save(model.state_dict(), PATH)
